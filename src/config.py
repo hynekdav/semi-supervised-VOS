c00b96d5d22218ca8d4c7f2c6028b983755ef277
Hynek Davidek <davidhyn@fel.cvut.cz>
2020-09-27 12:36:59 +0200

    Faster evaluation

--- src/config.py
+++ src/config.py
@@ -2,6 +2,8 @@
 # ! python3
 
 
+import multiprocessing
+
 import torch
 
 
@@ -9,3 +11,4 @@ class Config(object):
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     SCALE = 0.125
     CONTINUOUS_FRAME = 4
+    CPU_COUNT = max(multiprocessing.cpu_count() - 2, 1)
--- src/evaluation.py
+++ src/evaluation.py
@@ -9,7 +9,16 @@ from loguru import logger
 from skimage.io import imread
 from tqdm import tqdm
 
+from multiprocessing import Pool
+
+from src.config import Config
 from src.utils.metrics import evaluate_segmentation
+
+
+def process_pair(gt, seg):
+    gt_img = np.ceil(imread(str(gt), as_gray=True))
+    seg_img = np.ceil(imread(str(seg), as_gray=True))
+    return evaluate_segmentation(gt_img, seg_img)
 
 
 @click.command(name='evaluation')
@@ -21,12 +30,18 @@ def evaluation_command(ground_truth, computed_results):
     computed = Path(computed_results)
     ground_truth = Path(ground_truth)
 
-    total = len(list(ground_truth.glob('**/*.png')))
+    ground_truth = list(ground_truth.glob('**/*.png'))
+    computed = list(computed.glob('**/*.png'))
+    total = len(ground_truth)
+
+    assert len(ground_truth) == len(computed)
+
     logger.info(f'Staring evaluation on {total} pairs.')
-    scores = []
-    for gt, seg in tqdm(zip(ground_truth.glob('**/*.png'), computed.glob('**/*.png')), total=total):
-        gt_img = np.ceil(imread(str(gt), as_gray=True))
-        seg_img = np.ceil(imread(str(seg), as_gray=True))
-        scores.append(evaluate_segmentation(gt_img, seg_img))
+    pbar = tqdm(total=total)
+    with Pool(Config.CPU_COUNT) as pool:
+        res = [pool.apply_async(process_pair, args=(gt, seg,), callback=lambda _: pbar.update(1)) for gt, seg in
+               list(zip(ground_truth, computed))]
+        scores = [p.get() for p in res]
+        pbar.close()
     j, f = map(np.array, zip(*scores))
     logger.info(f'Evaluated: j_mean={j.mean()}, f_mean={f.mean()}.')
