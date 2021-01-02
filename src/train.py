c28d3f1c9985029f9d3d17b8375e4249b8a33167
Hynek Davidek <davidhyn@fel.cvut.cz>
2021-01-02 16:46:20 +0100

    Fix triplet loss

--- src/train.py
+++ src/train.py
@@ -57,8 +57,9 @@ def train_command(frame_num, data, resume, save_model, epochs, model, temperatur
     else:
         if loss == 'triplet':
             # todo prepsat to z triplet margin loss na klasickou triplet loss
-            criterion = losses.TripletMarginLoss(distance=distance, margin=0.0).to(Config.DEVICE)
-            miner = miners.TripletMarginMiner(type_of_triplets='hard')
+            criterion = losses.TripletMarginLoss(distance=distance, margin=0.0, triplets_per_anchor=128).to(
+                Config.DEVICE)
+            miner = None  # miners.TripletMarginMiner(type_of_triplets='hard')
         else:
             criterion = losses.ContrastiveLoss(pos_margin=0.1, neg_margin=0.9, distance=distance).to(Config.DEVICE)
             miner = miners.PairMarginMiner(pos_margin=0.1, neg_margin=0.9)
@@ -145,14 +146,11 @@ def train_alternative(train_loader, model, criterion, miner, optimizer, epoch, c
 
         features = F.normalize(features, p=2, dim=1)
 
-        if isinstance(miner, miners.TripletMarginMiner):
-            indices = torch.randint(low=0, high=features.shape[0], size=(256,), device=Config.DEVICE)
-            features = features.index_select(0, indices)
-            labels = labels.index_select(0, indices)
-
-        miner_output = miner(features, labels)
-
-        loss = criterion(features, labels, miner_output)
+        if miner is not None:
+            miner_output = miner(features, labels)
+            loss = criterion(features, labels, miner_output)
+        else:
+            loss = criterion(features, labels)
         mean_loss.append(loss.item())
         loss.backward()
 
