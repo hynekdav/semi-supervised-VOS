1b308e1ad88491986f5d4b6d95c606e422984b3c
Hynek Davidek <davidhyn@fel.cvut.cz>
2021-01-26 18:49:21 +0100

    Selecting point coordinates for heatmap

--- src/visualization/heatmap.py
+++ src/visualization/heatmap.py
@@ -44,15 +44,15 @@ def set_device_and_load_model(checkpoint, device):
     return model
 
 
-def get_similarity_matrix(features):
+def get_similarity_matrix(features, x, y):
     (_, _, H, W) = features.shape
+    idx = np.round(x / 8 + y / 8 * W).astype(int)
     features = features.reshape(256, -1).permute(1, 0)
     features_norm = torch.nn.functional.normalize(features, p=1, dim=1)
-    idx = torch.randint(low=500, high=features.shape[0] - 500, size=(1,))
     similarity = 1 - torch.cdist(features_norm[idx].reshape(1, -1), features_norm, p=1).squeeze().detach()
     similarity[idx] = 2
     similarity = similarity.reshape(H, W)
-    return similarity, idx
+    return similarity
 
 
 @click.command(name='heatmap')
@@ -65,11 +65,13 @@ def get_similarity_matrix(features):
 @click.option('--device', type=click.Choice(['cpu', 'cuda']), default='cuda', help='Device to run computing on.')
 @click.option('--save/--no-save', default=False, help='Save the image or show it.')
 @click.option('-s', '--save_path', help='Path to save image to.')
-def heatmap_command(image, annotation, checkpoint, device, save, save_path):
-    heatmap_command_impl(image, annotation, checkpoint, device, save, save_path)
+@click.option('-x', type=click.IntRange(min=0, max=854), help='X coordinate of point to select.', required=True)
+@click.option('-y', type=click.IntRange(min=0, max=480), help='Y coordinate of point to select.', required=True)
+def heatmap_command(image, annotation, checkpoint, device, save, save_path, x, y):
+    heatmap_command_impl(image, annotation, checkpoint, device, save, save_path, x, y)
 
 
-def heatmap_command_impl(image, annotation, checkpoint, device, save, save_path):
+def heatmap_command_impl(image, annotation, checkpoint, device, save, save_path, x, y):
     image_path = Path(image)
     model = set_device_and_load_model(checkpoint, device)
     annotation = remove_black_alpha(Image.open(annotation).convert('RGBA'))
@@ -81,8 +83,7 @@ def heatmap_command_impl(image, annotation, checkpoint, device, save, save_path)
     image = Image.open(image).convert('RGB')
     image_normalized = rgb_normalize(np.asarray(image)).unsqueeze(0).to(Config.DEVICE)
     features_tensor: torch.Tensor = model(image_normalized).detach().cpu()
-    similarity, idx = get_similarity_matrix(features_tensor)
-    x, y = int((idx % features_tensor.shape[3]) * 8), int((idx / features_tensor.shape[3]) * 8)
+    similarity = get_similarity_matrix(features_tensor, x, y)
     fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(3 * 8.1, 4.5))
     fig.suptitle(f'Heatmap - {image_path.parent.stem} frame: {image_path.stem.title()}\nSelected index [{x}, {y}]')
     ax[0].imshow(annotation)
