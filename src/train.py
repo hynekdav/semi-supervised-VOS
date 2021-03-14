8134d8b68e89c2865a775d2d1e4a7c3a88c1f178
Hynek Davidek <davidhyn@fel.cvut.cz>
2021-03-14 11:01:42 +0100

    Fix manual seed

--- src/train.py
+++ src/train.py
@@ -128,6 +128,7 @@ def train_command(frame_num, training, validation, resume, save_model, epochs, b
 
     early_stopper = EarlyStopping(save_model, trace_func=logger.info, verbose=True)
     for epoch in tqdm(range(start_epoch, start_epoch + epochs), desc='Training.'):
+        torch.manual_seed(42)
         train_loss = step(train_loader, model, criterion, optimizer, epoch, centroids, train_batches, mode='train')
         # validation_loss = step(validation_loader, model, criterion, None, epoch, centroids, validation_batches,
         #                       mode='val')
