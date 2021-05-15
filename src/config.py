d592aba769fa67352e105eec670c14f854bb79b6
Hynek Davidek <davidhyn@fel.cvut.cz>
2021-05-16 00:13:02 +0200

    Increase cpu count

--- src/config.py
+++ src/config.py
@@ -11,4 +11,4 @@ class Config(object):
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     SCALE = 0.125
     CONTINUOUS_FRAME = 4
-    CPU_COUNT = max(multiprocessing.cpu_count() - 2, 1)
+    CPU_COUNT = max(multiprocessing.cpu_count(), 1)
